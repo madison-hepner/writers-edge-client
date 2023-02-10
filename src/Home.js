744d2419feb75b97ea048b3faf3f2b33b9613806
madison-hepner <hepnermadison@icloud.com>
2023-02-10 12:55:17 -0600

    set up

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { SearchBar } from "./components/search/SearchBar";
+import { EventList } from "./components/events/EventList";
+import "./Home.css"
+
+export const Home = () => (
+    <>
+        <h2 className="title">Writers Edge</h2>
+        <small>Prompt and Writing Tool</small>
+
+        <address>
+            <div>more stuff here</div>
+        </address>
+
+
+    </>
+)
